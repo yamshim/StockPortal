eb4165d0bbf58f7a59ba0dc2abb2be3e7dc80916
akasatana <akasatana@akasatana-no-MacBook-Air.local>
2015-10-20 20:36:29 +0900

    initialized_data3

--- Gemfile
+++ Gemfile
@@ -5,20 +5,27 @@ source 'https://rubygems.org'
 
 gem 'rails', '4.2.3'
 gem 'mysql2', '~> 0.3.18'
-gem 'pdf-reader'
 gem 'selenium-webdriver', '2.46.2'
 gem 'whenever', require: false
-gem 'capistrano', '~> 3.4.0'
-
+gem 'pdf-reader'
+gem 'unicorn', '4.9.0'
 # for Logger
 gem 'ltsv'
 gem 'ltsvr'
 gem 'fluent-logger', '~> 0.4.9'
 
 group :development, :test do
-gem 'pry'
-gem 'pry-rails'
-gem 'pry-remote'
+  gem 'pry'
+  gem 'pry-rails'
+  gem 'pry-remote'
+end
+
+group :deployment do
+  gem 'capistrano', '~> 3.2.1'
+  gem 'capistrano-rails'
+  gem 'capistrano-rbenv'
+  gem 'capistrano-bundler'
+  gem 'capistrano3-unicorn' # unicornを使っている場合のみ
 end
 
 group :assets do
---
+++ config/deploy/production.rb
@@ -0,0 +1,46 @@
+# Simple Role Syntax
+# ==================
+# Supports bulk-adding hosts to roles, the primary server in each group
+# is considered to be the first unless any hosts have the primary
+# property set.  Don't declare `role :all`, it's a meta role.
+
+role :app, %w{akasatana@153.126.139.250}
+role :web, %w{akasatana@153.126.139.250}
+role :db,  %w{akasatana@153.126.139.250}
+
+
+# Extended Server Syntax
+# ======================
+# This can be used to drop a more detailed server definition into the
+# server list. The second argument is a, or duck-types, Hash and is
+# used to set extended properties on the server.
+
+server '153.126.139.250', user: 'akasatana', roles: %w{web app db}
+
+
+# Custom SSH Options
+# ==================
+# You may pass any option but keep in mind that net/ssh understands a
+# limited set of options, consult[net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start).
+#
+# Global options
+# --------------
+#  set :ssh_options, {
+#    keys: %w(/home/rlisowski/.ssh/id_rsa),
+#    forward_agent: false,
+#    auth_methods: %w(password)
+#  }
+#
+# And/or per server (overrides global)
+# ------------------------------------
+# server 'example.com',
+#   user: 'user_name',
+#   roles: %w{web app},
+ssh_options: {
+  user: 'akasatana', # overrides user setting above
+  keys: %w(/Users/akasatana/.ssh/id_rsa),
+  forward_agent: true,
+  auth_methods: %w(publickey),
+  port: '61203'
+#     # password: 'please use keys'
+}
